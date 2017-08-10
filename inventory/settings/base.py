3dcb8f880885de34b9d21b8e8e01d4a60b25b31f
Tyler Hardy <tylerhardy@ad.weber.edu>
2017-08-10 13:18:03 -0600

    quick fix

--- inventory/settings/base.py
+++ inventory/settings/base.py
@@ -20,11 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
--- inventory/settings/development.py
+++ inventory/settings/development.py
@@ -5,4 +5,4 @@ from .base import *
 DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
