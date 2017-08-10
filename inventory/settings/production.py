06e45bef2eb22797eaed77c743e8add4f7cea64f
Tyler Hardy <tylerhardy@ad.weber.edu>
2017-08-10 13:12:00 -0600

    test secret time

--- inventory/settings/production.py
+++ inventory/settings/production.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*- 
 from .base import * 
 
+SECRET_KEY = '75jele&o5ftncyfx#0p&ttmjcl635r3j1r3oipv##b0q0o7o20'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
