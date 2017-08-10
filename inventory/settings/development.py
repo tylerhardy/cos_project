c8ae1c3cd6516a0a1b92647a6d78458c740c8a8f
Tyler Hardy <tylerhardy@ad.weber.edu>
2017-08-10 13:14:09 -0600

    Allowed Hosts

--- inventory/settings/development.py
+++ inventory/settings/development.py
@@ -3,3 +3,6 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+# SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = []
--- inventory/settings/production.py
+++ inventory/settings/production.py
@@ -5,3 +5,6 @@ SECRET_KEY = '75jele&o5ftncyfx#0p&ttmjcl635r3j1r3oipv##b0q0o7o20'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+# SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = ['137.190.117.94']
