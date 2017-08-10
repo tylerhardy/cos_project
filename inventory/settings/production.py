8d50c88a78994a4104df87f506c7dc8824f5b5a5
@tylerhardy <tylerhardy@gmail.com>
2017-08-10 13:36:07 -0600

    static settings

--- inventory/settings/production.py
+++ inventory/settings/production.py
@@ -3,3 +3,17 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cos_inv_db',
+        'USER': 'tylerhardy',
+        'PASSWORD': 'Letmein11',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
