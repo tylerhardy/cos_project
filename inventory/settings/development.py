86055f94f4667298b9ace9f2149b8a31960f19fa
Tyler Hardy <tylerhardy@ad.weber.edu>
2017-08-10 14:09:52 -0600

    removed un-needed files

--- inventory/settings/development.py
+++ inventory/settings/development.py
@@ -4,29 +4,21 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-<<<<<<< HEAD
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['localhost','127.0.0.1','137.190.116.87']
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-=======
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
->>>>>>> 8d50c88a78994a4104df87f506c7dc8824f5b5a5
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-<<<<<<< HEAD
 }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-=======
-}
->>>>>>> 8d50c88a78994a4104df87f506c7dc8824f5b5a5
+
--- inventory/settings/production.py
+++ inventory/settings/production.py
@@ -7,19 +7,11 @@ SECRET_KEY = '75jele&o5ftncyfx#0p&ttmjcl635r3j1r3oipv##b0q0o7o20'
 DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-<<<<<<< HEAD
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '137.190.117.94']
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-=======
-DEBUG = False
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
->>>>>>> 8d50c88a78994a4104df87f506c7dc8824f5b5a5
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -29,13 +21,9 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '',
     }
-<<<<<<< HEAD
 }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-=======
-}
->>>>>>> 8d50c88a78994a4104df87f506c7dc8824f5b5a5
