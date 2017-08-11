2098f24801ca87b6a9cb17c0cc5ee5fdf652a5ed
Tyler Hardy <tylerhardy@ad.weber.edu>
2017-08-11 10:45:08 -0600

    Production, debug false

--- inventory/settings/production.py
+++ inventory/settings/production.py
@@ -4,7 +4,7 @@ from .base import *
 SECRET_KEY = '75jele&o5ftncyfx#0p&ttmjcl635r3j1r3oipv##b0q0o7o20'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '137.190.117.94']
