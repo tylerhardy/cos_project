cb89415b2835e7db1425831d3eebe5e904755406
Tyler Hardy <tylerhardy@ad.weber.edu>
2017-08-11 12:49:45 -0600

    added ty128... hostname

--- inventory/settings/production.py
+++ inventory/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '137.190.117.94']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '137.190.117.94','ty128-rpi3.users.weber.edu']
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
